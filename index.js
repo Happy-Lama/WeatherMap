e740e7210f162be9a81d0b23447827769f05563b
Kissa Leon Denis <61694317+Happy-Lama@users.noreply.github.com>
2024-07-05 14:29:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'demo')));
 app.use(morgan('dev'))
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
